69121aef959c5d442314fb71f22065a975dbffab
JamesCarrollRob <46402913+JamesCarrollRob@users.noreply.github.com>
2022-11-13 16:05:51 -0500

    working on adding the option to change the color of grid square

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const container = document.getElementById("grid-container");
 const btn = document.getElementById("clear")
+colorChange = false;
 
 // Clears darkened grid squares
 function mouseOver2(){
@@ -29,18 +30,56 @@ function mouseOver2(){
     console.log(count);
   }
   
-
-//calls grid function
-  createGrid();
-
-  // Adds event listener for grid squares
-  let boxes  = document.querySelectorAll(".grid-element");
+  function boxListener(){
+    let boxes  = document.querySelectorAll(".grid-element");
   boxes.forEach(item => {
     item.addEventListener('mouseover', event => {
       //handle click
       item.style.backgroundColor = "black";
     })
+  }) 
+  }
+
+//calls grid function
+  createGrid();
+  boxListener();
+
+  // function changeColor(){
+  //   let boxes  = document.querySelectorAll(".grid-element");
+  // boxes.forEach(item => {
+  //   item.removeEventListener
+  //   item.addEventListener('mouseover', event => {
+  //     //handle click
+  //     item.style.backgroundColor = "black";
+  //   })
+  // })
+  // }
+
+  // Adds event listener for grid squares
+  // if (colorChange != true){
+  // let boxes  = document.querySelectorAll(".grid-element");
+  // boxes.forEach(item => {
+  //   item.addEventListener('mouseover', event => {
+  //     //handle click
+  //     item.style.backgroundColor = "black";
+  //   })
+  // }) 
+  // } else {
+  //   let boxes  = document.querySelectorAll(".grid-element");
+  //   boxes.forEach(item => {
+  //     removeEventListener('mouseover')
+  //   })
+  // }
+  function changeColor(){
+    colorChange = !colorChange;
+  }
+
+  let colorOpt = document.getElementById("change-color")
+  colorOpt.addEventListener('click', e => {
+   changeColor();
+    console.log("you clicked the button");
   })
+
 
 
  console.log("hello");
